f3367d3e7e47aca69a9235331ef355c1fbb375c5
Anna Velarde <annavelardevilaseca@gmail.com>
2023-04-25 20:56:56 +0100

    compiling ts file with javascript file

---
+++ sandbox.js
@@ -0,0 +1,4 @@
+var character = "anna";
+console.log(character);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
---
+++ sandbox.ts
@@ -0,0 +1,5 @@
+const character = 'anna';
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
