d8666156d60eb6aaffee1a7b0c18befd409abcdd
Anna Velarde <annavelardevilaseca@gmail.com>
2023-04-27 19:25:10 +0100

    lesson-4-arrays&objects

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,29 @@
-var character = 'anna';
-var age = 30;
-var Bristol = false;
-//character. I can't swap name 'anna' for number
-character = 'hello';
-console.log(character);
-// const circle=(diameter)=>{return diameter *Math.PI} 
-// console.log(circle(7.5));
-var circle = function (diameter) { return diameter * Math.PI; };
-console.log(circle(7.5));
+// arrays
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+// names.push(3);
+// names[1] = 3;
+var numbers = [10, 20, 12, 15];
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+// objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking']
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40,
+    // skills: ['running'],
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,12 +1,37 @@
-let character = 'anna'
-let age = 30;
-let Bristol=false;
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
 
-//character. I can't swap name 'anna' for number
-character= 'hello'
-console.log(character);
+names.push('toad');
+// names.push(3);
+// names[1] = 3;
 
-// const circle=(diameter)=>{return diameter *Math.PI} 
-// console.log(circle(7.5));
-const circle=(diameter:number)=>{return diameter *Math.PI} 
-console.log(circle(7.5));
+let numbers = [10, 20, 12, 15];
+
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+
+// objects
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30
+};
+
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking']
+
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 40,
+  // skills: ['running'],
+};
