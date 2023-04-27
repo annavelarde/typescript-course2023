0427c1a11f3862c8c6392783c55bf30cb595a3a1
Anna Velarde <annavelardevilaseca@gmail.com>
2023-04-27 19:08:20 +0100

    lesson-3

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,10 @@
 var character = 'anna';
+var age = 30;
+var Bristol = false;
+//character. I can't swap name 'anna' for number
+character = 'hello';
 console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
+// const circle=(diameter)=>{return diameter *Math.PI} 
+// console.log(circle(7.5));
+var circle = function (diameter) { return diameter * Math.PI; };
+console.log(circle(7.5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,5 +1,12 @@
-const character = 'anna';
+let character = 'anna'
+let age = 30;
+let Bristol=false;
+
+//character. I can't swap name 'anna' for number
+character= 'hello'
 console.log(character);
 
-const inputs = document.querySelectorAll('input');
-console.log(inputs);
+// const circle=(diameter)=>{return diameter *Math.PI} 
+// console.log(circle(7.5));
+const circle=(diameter:number)=>{return diameter *Math.PI} 
+console.log(circle(7.5));
