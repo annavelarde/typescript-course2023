174bc6187e100efe60ba837906aa12a1d3fb9969
Anna Velarde <annavelardevilaseca@gmail.com>
2023-04-27 20:02:56 +0100

    Explicit types and declaration/initialization

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,25 @@
+//initialization
+var character = 'mario';
+var age;
+var isLoggedIn;
+// age = 'luigi'; declaring the variable
+age = 30;
+// isLoggedIn = 25;
+isLoggedIn = true;
+// arrays
 var ninjas = [];
-console.log(typeof ninjas);
+ninjas.push('ryu');
+ninjas.push('chun-li');
+console.log(ninjas);
+// union types. Add brackets in front of array, otherwise it is not need it.
+var mixed = [];
+mixed.push('hello');
+mixed.push(false);
+mixed.push(20);
+console.log(mixed);
+var uid;
+// objects
+var ninjaOne;
+ninjaOne = { name: 'yoshi', age: 30 };
+var ninjaTwo;
+ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
--- sandbox.ts
+++ sandbox.ts
@@ -1,2 +1,37 @@
- let ninjas:string[]=[]
- console.log(typeof ninjas);
+//initialization
+let character: string = 'mario';
+let age: number;
+let isLoggedIn: boolean;
+
+// age = 'luigi'; declaring the variable
+age = 30;
+
+// isLoggedIn = 25;
+isLoggedIn = true;
+
+// arrays
+let ninjas: string[] = [];
+
+ninjas.push('ryu');
+ninjas.push('chun-li');
+console.log(ninjas);
+
+// union types. Add brackets in front of array, otherwise it is not need it.
+let mixed: (string|number|boolean)[] = [];
+mixed.push('hello');
+mixed.push(false);
+mixed.push(20);
+console.log(mixed);
+
+let uid: string|number;
+
+// objects
+let ninjaOne: object;
+ninjaOne = { name: 'yoshi', age: 30 };
+
+let ninjaTwo: {
+  name: string,
+  age: number,
+  beltColour: string
+};
+ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
