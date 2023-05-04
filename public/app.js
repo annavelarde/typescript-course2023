ebf1e6a453c52cdd62e0f64f7efd415df1ec5946
Anna Velarde <annavelardevilaseca@gmail.com>
2023-05-04 12:06:05 +0100

    classes lesson-12

--- public/app.js
+++ public/app.js
@@ -1,22 +1,32 @@
 "use strict";
-// The DOM & Type Casting
-// const anchor = document.querySelector('a');
-// console.log(anchor.href) 
-//comment:typescript doesnt recognise the consolelog so that's why we do a check
-//First check that works
-//if (Anchor){console.log(Anchor.href)}
-//Second check if we are sure that we have an 'a' in the HTML... we add in line 3 a '!'
-const anchor = document.querySelector('a');
-console.log(anchor.href);
-//let's try to get the form using DOM
-//if you have more than one form in the HTML element
-//const form= document.querySelector('form)!;  //tyoe HTMLFormElement
-//for more than one form in HTML element
-// const form =document.querySelector('.new-item-form')!; //type Element
-//to specify the type of element... below
+// Lesson 12 = Classes
+//Classes. Instead of using camelCase we use pascal. It means to add a capital.
+class Invoice {
+    //this function inside a class it´s called method
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details} `;
+    }
+}
+//we use the reserved word "new" to create new objects
+const invOne = new Invoice('anna', 'work in coding', 250);
+const invTwo = new Invoice('juan', 'work in coding too', 250);
+console.log(invOne, invTwo);
 const form = document.querySelector('.new-item-form');
-console.log(form.children);
-//inputs. // using "id"
+//if we want to them in an array
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+//We can specify the values too below and update some of the values above.
+invOne.client = 'Yan';
+invTwo.amount = 30;
+console.log(invoices);
+//---------
+//inputs. 
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
@@ -25,4 +35,3 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
-//we use valueAsNumber instead of value because we don´t want the number to be an string.
--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,58 @@
-// The DOM & Type Casting
+// Lesson 12 = Classes
 
-// const anchor = document.querySelector('a');
-// console.log(anchor.href) 
-//comment:typescript doesnt recognise the consolelog so that's why we do a check
+//Classes. Instead of using camelCase we use pascal. It means to add a capital.
+class Invoice{
+    //typescript
+    client:string;
+    details:string;
+    amount:number;
 
-//First check that works
-//if (Anchor){console.log(Anchor.href)}
+    //this function inside a class it´s called method
+    constructor(c:string, d:string, a:number){
+        this.client =c;
+        this.details=d;
+        this.amount =a;
+    }
+    
+    format(){
+    return`${this.client} owes ${this.amount} for ${this.details} `;
+    }
+}
 
-//Second check if we are sure that we have an 'a' in the HTML... we add in line 3 a '!'
+//we use the reserved word "new" to create new objects
+const invOne= new Invoice('anna','work in coding', 250);
+const invTwo= new Invoice('juan','work in coding too', 250);
 
-const anchor = document.querySelector('a')!;
-console.log(anchor.href) 
+console.log(invOne, invTwo);
 
-//let's try to get the form using DOM
-
-//if you have more than one form in the HTML element
-//const form= document.querySelector('form)!;  //tyoe HTMLFormElement
-
-//for more than one form in HTML element
-// const form =document.querySelector('.new-item-form')!; //type Element
-//to specify the type of element... below
 const form =document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
 
-//inputs. // using "id"
+//if we want to them in an array
+let invoices: Invoice[]=[]
+invoices.push(invOne);
+invoices.push(invTwo)
+
+//We can specify the values too below and update some of the values above.
+invOne.client='Yan';
+invTwo.amount=30;
+
+console.log(invoices);
+
+//---------
+
+//inputs. 
 const type=document.querySelector('#type') as HTMLSelectElement
 const tofrom=document.querySelector('#tofrom') as HTMLInputElement
 const details=document.querySelector('#details') as HTMLInputElement
 const amount=document.querySelector('#amount') as HTMLInputElement
 
-form.addEventListener('submit',(e:Event)=>{e.preventDefault();
-console.log(type.value,tofrom.value,details.value,amount.valueAsNumber);})
+form.addEventListener('submit',(e:Event)=>{
+    e.preventDefault();
 
-//we use valueAsNumber instead of value because we don´t want the number to be an string.
+    console.log(
+        type.value,
+        tofrom.value,
+        details.value,
+        amount.valueAsNumber)
+;})
+
